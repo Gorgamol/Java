9fe28d6d96d90e36c6d3b9a106e4deda0e763312
Gorgamol <patrick.katthose@googlemail.com>
2016-12-03 23:06:59 +0100

    03.12.2016

--- Studium/Praktikum_20161128/src/Mäxchen.java
+++ Studium/Praktikum_20161128/src/Mäxchen.java
@@ -7,27 +7,55 @@ public class Mäxchen {
 		return (int) ((Math.random() * 6) + 1);
 	}
 	
+	public static void zwischenStand(ArrayList<Spieler> arr) {
+		int temp = arr.get(0).getPunkte();
+		int index = 0;
+		
+		for(int i = 0; i < arr.size(); ++i) {
+			if(arr.get(i).getPunkte() > temp) {
+				index = i;
+			}
+		}
+		System.out.println("------------------------------------------------");
+		System.out.println("Der momentan führende Spieler ist " + arr.get(index).getSpielerName() + " mit " + arr.get(index).getPunkte() + " Punkten!");
+	}
+	
+	public static void sieger(ArrayList<Spieler> arr) {
+		int temp = arr.get(0).getPunkte();
+		int index = 0;
+		
+		for(int i = 0; i < arr.size(); ++i) {
+			if(arr.get(i).getPunkte() > temp) {
+				index = i;
+			}
+		}
+		System.out.println("\n\nDer Sieger des Spiels ist " + arr.get(index).getSpielerName() + " mit " + arr.get(index).getPunkte() + " Punkten!, Herzlichen Glückwunsch");	
+	}
+	
 	public static void main(String[] args) {
 
-		
 		ArrayList<Spieler> spielerListe = new ArrayList<Spieler>();
-		
-		Scanner sc = new Scanner(System.in);		
+		System.out.println("Hallo! Willkommen zum Spiel Mäxchen!");
+		System.out.println(" > Bitte geben Sie die Anzahl der Spieler an:");
+		Scanner sc = new Scanner(System.in);
 		int spielerZahl = Integer.parseInt(sc.nextLine());
-		String spielerName;
 		for(int i = 0; i < spielerZahl; ++i) {
-			spielerName = sc.nextLine();
-			spielerListe.add(new Spieler("[" + spielerName + "]"));
+			System.out.println("\n > Geben Sie den Namen von Spieler " + (i + 1) + " ein:");
+			spielerListe.add(new Spieler("[" + sc.nextLine() + "]"));
 		}
 		sc.close();
 		
 		for(int i = 0; i < 10; ++i) {
+			System.out.println("\n>>>>>>>>>>>>>>>>>>>>[RUNDE " + (i + 1) + "]<<<<<<<<<<<<<<<<<<<<");
 			for(int j = 0; j < spielerListe.size(); ++j) {
 				System.out.println(spielerListe.get(j).getSpielerName() + " ist an der Reihe!");
 				spielerListe.get(j).setPunkte(würfeln(), würfeln());
 			}
+			zwischenStand(spielerListe);
 		}
-
+		sieger(spielerListe);
+		
+		
 	}
 
 }
--- Studium/Praktikum_20161128/src/Spieler.java
+++ Studium/Praktikum_20161128/src/Spieler.java
@@ -21,7 +21,23 @@ public class Spieler {
 	
 	public void setPunkte(int wurf_1, int wurf_2) {
 		System.out.println(this.getSpielerName() + " hat eine [" + wurf_1 + "] und eine [" + wurf_2 + "] gewürfelt!");
-		this.punkte = punkte;
+		int temp = 0;
+		if(wurf_1 == 1 && wurf_2 == 2) {
+			temp = 1000;
+			this.punkte += temp;
+		} else if(wurf_1 == wurf_2) {
+			temp = wurf_1 * 100;
+			this.punkte += temp;
+		} else if(!(wurf_1 == 1 && wurf_2 == 2) && !(wurf_1 == wurf_2)) {
+			if(wurf_1 > wurf_2) {
+				temp = 10 * wurf_1 + wurf_2;
+				this.punkte += temp;
+			} else if(wurf_2 > wurf_1) {
+				temp = 10 * wurf_2 + wurf_1;
+				this.punkte += temp;
+			}
+		}
+		System.out.println(this.getSpielerName() + " bekommt " + temp + " Punkte!");
 	}
 
 }
