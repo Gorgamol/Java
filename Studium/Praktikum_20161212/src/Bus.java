91a7636f818a3fa9d9de0c330568f45ac30b3991
Gorgamol <patrick.katthose@googlemail.com>
2016-12-12 09:19:17 +0100

    12.12.2016

---
+++ Studium/Praktikum_20161212/src/Bus.java
@@ -0,0 +1,28 @@
+
+public class Bus extends Fahrzeug{
+	private int sitzplätze;
+	
+	public Bus(String kennzeichen, int sitzplätze) {
+		super(kennzeichen);
+		this.sitzplätze = sitzplätze;
+	}
+	
+	public double getSitzplätze() {return this.sitzplätze;}
+	
+	public double berechneSteuer() {
+		return 800.00 + (this.getSitzplätze() * 5);
+	}
+
+	public boolean equals(Object o) {
+		if(o instanceof Bus) {
+			if(((Bus)o).getKennzeichen() == this.getKennzeichen()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String toString() {
+		return "LKW - [" + this.getKennzeichen() + "] | Nutzlast - [" + this.getSitzplätze() + "]";
+	}
+}
---
+++ Studium/Praktikum_20161212/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Handyvertrag hv = new Handyvertrag();
+		
+		Angebot angebot_1 = new Angebot(27.00, 0.14, 45);
+		Angebot angebot_2 = new Angebot(30.00, 0.13, 50);
+		Angebot angebot_3 = new Angebot(5.00, 0.30, 0);
+		Angebot angebot_4 = new Angebot(10.00, 0.08, 20);
+		
+		hv.einfügen(angebot_1);
+		hv.einfügen(angebot_2);
+		hv.einfügen(angebot_3);
+		//hv.einfügen(angebot_4);
+		
+		hv.listeAngebote();
+		
+		System.out.println(hv.günstig(50));
+
+	}
+
+}
